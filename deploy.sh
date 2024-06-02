1cc94351fef45912cf47ac2468992a79fdd46925
alichowdhury01 <39740365+alichowdhury01@users.noreply.github.com>
2024-06-02 06:51:05 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Navigate to the project directory
-cd /root/var/www/portfolio/frontend
+cd /root/var/www/html/resume
 
 echo "Pulling latest changes from repository"
 git pull origin main
