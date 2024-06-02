378f43ed9f740701c9af8afdce2dde7ec31d0d9c
alichowdhury01 <39740365+alichowdhury01@users.noreply.github.com>
2024-06-02 07:02:43 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextMDX = require("@next/mdx");
-const remarkGfm = require("remark-gfm");
-const rehypePrism = require("@mapbox/rehype-prism");
+import nextMDX from "@next/mdx";
+import remarkGfm from "remark-gfm";
+import rehypePrism from "@mapbox/rehype-prism";
 
 const nextConfig = {
   images: {
@@ -12,18 +12,12 @@ const nextConfig = {
   },
 };
 
-const withMDX = async () => {
-  const nextMDX = (await import("@next/mdx")).default;
-  const remarkGfm = (await import("remark-gfm")).default;
-  const rehypePrism = (await import("@mapbox/rehype-prism")).default;
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypePrism],
+  },
+});
 
-  return nextMDX({
-    extension: /\.mdx?$/,
-    options: {
-      remarkPlugins: [remarkGfm],
-      rehypePlugins: [rehypePrism],
-    },
-  })(nextConfig);
-};
-
-module.exports = withMDX();
+module.exports = withMDX(nextConfig);
