e49c2f36aad752c0e63c01cf5a73fbad00354d6a
alichowdhury01 <39740365+alichowdhury01@users.noreply.github.com>
2024-06-03 19:42:23 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 import nextMDX from '@next/mdx';
 import remarkGfm from 'remark-gfm';
 import rehypePrism from '@mapbox/rehype-prism';
+import sharp from 'sharp';
 
 const withMDX = nextMDX({
   extension: /\.mdx?$/,
@@ -14,6 +15,8 @@ const withMDX = nextMDX({
 const nextConfig = {
   images: {
     domains: ["images.unsplash.com", "res.cloudinary.com", "www.alichowdhury.ca", "147.182.153.61"],
+    // path: '/_next/image',
+    path: ['/_next/image', '/_next/images', '/public/image/', '/_next/public/images/'],
   },
   experimental: {
     mdxRs: true,
